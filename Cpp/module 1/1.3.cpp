97fb02514753bb48e39a1786d068789202f36986
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-17 02:03:56 +0100

    How to take input in C++, Typecasting

---
+++ Cpp/module 1/1.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    char c;
+    cin >> x >>c;
+    cout << x << " " << c << endl << (char)x << " " << (int)c << endl;
+    return 0;
+}
