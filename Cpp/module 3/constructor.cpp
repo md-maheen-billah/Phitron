bc788bb0fa8625f7b03bd001d833d75162af1bfd
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-20 10:57:11 +0100

    Constructor and its Simulation

---
+++ Cpp/module 3/constructor.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int r, int c, double g)
+    {
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
+};
+
+int main()
+{
+    int r, c;
+    double g;
+    cin >> r >> c >> g;
+    Student rahim(40, 5, 3.3);
+    Student karim(r, c, g);
+
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+    return 0;
+}
