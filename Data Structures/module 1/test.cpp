fa20f1657b8b22a413d19db3152305d58a7567ca
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-08 00:35:47 +0100

    Space Complexity

--- Data Structures/module 1/test.cpp
+++ Data Structures/module 1/test.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 int main()
 {
-    int n;
+    int n; // here space complixity O(1)
     cin >> n;
+    int sum = 0; // here space complixity O(1)
+    int a[n];    // however here space complixity is O(N)
+    int b[n][n]; // however here space complixity is O(N*N)
 
     // using loop
-    for (int i = 0; i < n; i++) // here complexity O(N)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j < n; j*=2) // here complexity O(logN) but this loop is nested inside another loop
-        {
-            cout << "Hello" << endl;
-        }
+        cin >> a[i] >> b[i][i];
     }
-    // here overall complexity O(NlogN)
-    
+
+    cout << sum << endl;
 
     return 0;
 }
+
+// therefore we can see where space complixity effetcs most
