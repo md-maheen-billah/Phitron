548f57af3b5acdc0220dd62d3f3f67f020d67b31
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-08 00:04:43 +0100

    Linearithmic complexity O(NlogN)

--- Data Structures/module 1/Linearithmic complexity O(NlogN).cpp
+++ Data Structures/module 1/Linearithmic complexity O(NlogN).cpp
@@ -8,13 +8,12 @@ int main()
     // using loop
     for (int i = 0; i < n; i++) // here complexity O(N)
     {
-        for (int j = 1; j < n; j*=2) // here complexity O(logN) but this loop is nested inside another loop
+        for (int j = 1; j < n; j *= 2) // here complexity O(logN) but this loop is nested inside another loop
         {
             cout << "Hello" << endl;
         }
     }
     // here overall complexity O(NlogN)
-    
 
     return 0;
 }
