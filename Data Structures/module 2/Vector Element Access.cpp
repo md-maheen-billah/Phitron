ccdb51d59883d46139311e96596a650521183990
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-10 05:22:56 +0100

    Vector Element Access

---
+++ Data Structures/module 2/Vector Element Access.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+
+    cout << v[3] << endl;
+    cout << v.back() << endl;
+    cout << v.front() << endl;
+
+    return 0;
+}
