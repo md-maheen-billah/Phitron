ec38e40deea24e8fe59473a2f6b3226900e36347
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-10 05:31:38 +0100

    Vector input and output

--- Data Structures/module 2/Vector input and output.cpp
+++ Data Structures/module 2/Vector input and output.cpp
@@ -4,20 +4,20 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> v(n); //if size is declared using n follow this method
+    vector<int> v(n); // if size is declared using n follow this method
     for (int i = 0; i < n; i++)
     {
         cin >> v[i];
     }
 
-    vector<int> v; //if size is not declared follow push_back method
+    vector<int> v; // if size is not declared follow push_back method
     for (int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
         v.push_back(x);
     }
-    
+
     for (auto it = v.begin(); it < v.end(); it++)
     {
         cout << *it << " ";
