21975d48bb16f8a3799bba89491b976a9c53e6e0
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-10 05:22:28 +0100

    Vector Element Access

--- Data Structures/module 2/test.cpp
+++ Data Structures/module 2/test.cpp
@@ -2,37 +2,11 @@
 using namespace std;
 int main()
 {
-    vector<int> v = {1, 2, 3, 2, 5, 2};
-    replace(v.begin(), v.end(), 2, 100); // denoate the range and the number to replace, in this case we replace 2 with 100
-    for (int x : v)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-
-
-
-    vector<int> v2 = {1, 2, 3, 2, 5, 2};
-    replace(v2.begin(), v2.end()-1, 2, 100); // denoate the range and the number to replace, in this case we replace first two 2 with 100
-    for (int x : v2)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-
-
-
-    auto it = find(v2.begin(), v2.end(), 4); // range and the number 
-    if(it==v2.end())
-    {
-        cout << "Not found";
-    }
-    else 
-    {
-        cout << "found";
-    }
-
-
+    vector<int> v = {1, 2, 3, 4, 5, 6};
     
+    cout << v[3] << endl;
+    cout << v.back() << endl;
+    cout << v.front() << endl;
+
     return 0;
 }
