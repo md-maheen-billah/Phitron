484ab6bbc7f76668ef9db3eaca7f6e139299540e
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-12 03:19:43 +0100

    Range Sum Query Bruteforce solution

---
+++ Data Structures/module 3/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+    vector<string> v(n); //if size is declared using n follow this method
+    for (int i = 0; i < n; i++)
+    {
+        getline(cin,v[i]);
+    }
+
+    for(string s:v)
+    {
+        cout << s << endl;
+    }
+    
+
+    return 0;
+}
