5313f548c9fab1dea0d51802e9acdbff0a8d9716
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-12 04:25:44 +0100

    Binary Search Implementation

---
+++ Data Structures/module 3/Binary Search Implementation.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+    int val;
+    cin >> val;
+    int flag = 0;
+
+    int l = 0;
+    int r = n - 1;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (v[mid] == val)
+        {
+            flag = 1;
+            break;
+        }
+        else if (v[mid] > val)
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    if (flag == 1)
+    {
+        cout << "Found";
+    }
+    else
+    {
+        cout << "Not Found";
+    }
+
+    return 0;
+}
