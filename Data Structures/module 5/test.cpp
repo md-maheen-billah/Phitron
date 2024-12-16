a525fcafc05977d35230adcc83fb038d9f51be53
md-maheen-billah <md.maheen.billah@icloud.com>
2024-12-16 21:23:28 +0100

    Creating a Node

--- Data Structures/module 5/test.cpp
+++ Data Structures/module 5/test.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+};
 int main()
 {
-    int n;
-    cin >> n;
-    cin.ignore();
-    vector<string> v(n); //if size is declared using n follow this method
-    for (int i = 0; i < n; i++)
-    {
-        getline(cin,v[i]);
-    }
+    Node a, b, c;
+    a.val = 10;
+    b.val = 20;
+    c.val = 30;
 
-    for(string s:v)
-    {
-        cout << s << endl;
-    }
-    
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    cout << a.val << endl;               // printing a
+    cout << a.next->val << endl;       // printing b using a
+    cout << a.next->next->val << endl; // printing c using a
 
     return 0;
 }
