54b550106bd2c090ff0b0c4b397c898ee5b10e3d
md-maheen-billah <md.maheen.billah@icloud.com>
2025-01-05 20:35:41 +0100

    Stack Implementation using Array II

--- Data Structures/module 13/Stack Implementation using Array I.cpp
+++ Data Structures/module 13/Stack Implementation using Array I.cpp
@@ -29,6 +29,27 @@ public:
 
 int main()
 {
+    myStack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout << st.top() << endl; // 30
+    st.pop();
+    cout << st.top() << endl; // 20
+    st.pop();
+    cout << st.top() << endl; // 10
+    // however if we keep popping and pop out everything and try to print the top, we will get a runtime error
+    // therefore we should first check if the stack is empty before printing
+    st.pop();
+    if (!st.empty())
+    {
+        cout << st.top() << endl; // nothing will be printed as stack is empty
+    }
+    // now if we try to implement pop again, we will face another error, therefore we should check whether a stack is empty before popping
+    if (!st.empty())
+    {
+        st.pop(); // nothing will be popped as stack is empty
+    }
 
     return 0;
 }
--- Data Structures/module 13/test.cpp
+++ Data Structures/module 13/test.cpp
@@ -3,32 +3,53 @@ using namespace std;
 
 class myStack
 {
-    public:
-        vector<int> v;
-        void push(int val)
-        {
-            v.push_back(val);
-        }
-        void pop()
-        {
-            v.pop_back();
-        }
-        int top()
-        {
-            return v.back();
-        }
-        int size()
-        {
-            return v.size();
-        }
-        bool empty()
-        {
-            return v.empty();
-        }
+public:
+    vector<int> v;
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
 };
 
 int main()
 {
-    
-   return 0;
+    myStack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout << st.top() << endl; // 30
+    st.pop();
+    cout << st.top() << endl; // 20
+    st.pop();
+    cout << st.top() << endl; // 10
+    // however if we keep popping and pop out everything and try to print the top, we will get a runtime error
+    // therefore we should first check if the stack is empty before printing
+    st.pop();
+    if (!st.empty())
+    {
+        cout << st.top() << endl; // nothing will be printed as stack is empty
+    }
+    // now if we try to implement pop again, we will face another error, therefore we should check whether a stack is empty before popping
+    if (!st.empty())
+    {
+        st.pop(); // nothing will be popped as stack is empty
+    }
+
+    return 0;
 }
