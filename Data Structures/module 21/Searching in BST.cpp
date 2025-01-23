e39f3c390ec6f5da2a9914514c4ed68c3f08363b
md-maheen-billah <md.maheen.billah@icloud.com>
2025-01-23 02:04:29 +0100

    Searching in BST

---
+++ Data Structures/module 21/Searching in BST.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. pop the node
+        Node *p = q.front();
+        q.pop();
+        // 2. take the input of the children for the node
+        int l, r;
+        cin >> l >> r;
+        Node *pleft, *pright;
+        if (l == -1)
+            pleft = NULL;
+        else
+            pleft = new Node(l);
+        if (r == -1)
+            pright = NULL;
+        else
+            pright = new Node(r);
+
+        p->left = pleft;
+        p->right = pright;
+        // 3. push the children of the node inside the queue
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+};
+
+bool search(Node *root, int val)
+{
+    if (root == NULL)
+        return false;
+    if (root->val == val)
+        return true;
+    if (root->val > val)
+        return search(root->left, val);
+    else
+        return search(root->right, val);
+}
+
+int main()
+{
+    Node *root = input_tree();
+    int val;
+    cin >> val;
+    if (search(root, val))
+        cout << "Found";
+    else
+        cout << "Not Found";
+    return 0;
+}
