070e297799ff71b8d63cc377994616a61fd9cb2b
md-maheen-billah <md.maheen.billah@icloud.com>
2025-02-08 09:41:09 +0100

    DFS on 2D grid

--- Algorithms/Module 3/test.cpp
+++ Algorithms/Module 3/test.cpp
@@ -1,30 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> adj_list[1005];
-bool vis[1005];
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
 
-void dfs(int src)
+bool valid(int i, int j)
 {
-    cout << src << " ";
-    vis[src] = true;
-    for (int child : adj_list[src])
-        if (vis[child] == false)
-            dfs(child);
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
 }
+
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci, cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+        if (valid(ci, cj) && !vis[ci][cj])
+            dfs(ci, cj);
+    }
+}
+
 int main()
 {
-    int n, e;
-    cin >> n >> e;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
 
-    while (e--)
-    {
-        int a, b;
-        cin >> a >> b;
-        adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
-    }
-    memset(vis, false, sizeof(vis)); // setting all values of vis to false
-    dfs(0);
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfs(si, sj);
 
     return 0;
 }
