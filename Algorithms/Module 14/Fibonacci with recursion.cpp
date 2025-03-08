458e772d7d550001a68fba2250564d273a138392
md-maheen-billah <md.maheen.billah@icloud.com>
2025-03-09 00:45:31 +0100

    Fibonacci with recursion

--- Algorithms/Module 14/Fibonacci with recursion.cpp
+++ Algorithms/Module 14/Fibonacci with recursion.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int fibonacci(int n)
 {
     if (n < 2)
-        return n; // if value of n is less than 2 then return the value.
+        return n; // if value of n is less than 2 then return the value
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main()
 {
-    cout << fibonacci(9);
+    cout << fibonacci(6);
     return 0;
 }
