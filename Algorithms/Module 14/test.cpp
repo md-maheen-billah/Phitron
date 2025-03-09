1f737cd62c2cae23fd513bdaa4cc852f3d096f4d
md-maheen-billah <md.maheen.billah@icloud.com>
2025-03-09 02:31:00 +0100

    Fibonacci with DP

--- Algorithms/Module 14/test.cpp
+++ Algorithms/Module 14/test.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibonacci(int n)
+int dp[1005];
+int fibonacci(int n) //using memoization
 {
     if (n < 2)
         return n; // if value of n is less than 2 then return the value.
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if(dp[n]!= -1)
+        return dp[n];
+    dp[n]= fibonacci(n - 1) + fibonacci(n - 2);
+    return dp[n];
 }
 
 int main()
 {
-    cout << fibonacci(9);
+    memset(dp,-1,sizeof(dp));
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
     return 0;
 }
